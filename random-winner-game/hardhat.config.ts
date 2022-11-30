5ffbcfbc51107b807de2c4b5e05e0ad5c40443bc
leowang <rfv7855659@gmail.com>
2022-11-30 17:49:51 +0800

    ✨add deploy script

--- random-winner-game/hardhat.config.ts
+++ random-winner-game/hardhat.config.ts
@@ -1,8 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const POLYGONSCAN_KEY = process.env.POLYGONSCAN_KEY;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    mumbai: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY!],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: POLYGONSCAN_KEY!,
+    },
+  },
 };
 
 export default config;
--- random-winner-game/scripts/deploy.ts
+++ random-winner-game/scripts/deploy.ts
@@ -1,18 +1,32 @@
 import { ethers } from "hardhat";
+require("dotenv").config({ path: ".env" });
+import { FEE, VRF_COORDINATOR, LINK_TOKEN, KEY_HASH } from '../constants'
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const randomWinnerGame = await ethers.getContractFactory('RandomWinnerGame');
+  const deployedRandomWinnerGameContract = await randomWinnerGame.deploy(
+    VRF_COORDINATOR,
+    LINK_TOKEN,
+    KEY_HASH,
+    FEE
+  )
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  await deployedRandomWinnerGameContract.deployed()
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  console.log("Verify Contract Address:", deployedRandomWinnerGameContract.address);
+  // Wait for etherscan to notice that the contract has been deployed
+  // await sleep(30000);
 
-  await lock.deployed();
+  // // Verify the contract after deploying
+  // await hre.run("verify:verify", {
+  //   address: deployedRandomWinnerGameContract.address,
+  //   constructorArguments: [VRF_COORDINATOR, LINK_TOKEN, KEY_HASH, FEE],
+  // });
+}
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
